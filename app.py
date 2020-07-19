af5f17d4ddcad745b70e7ffecdfd9d6f9ef66b16
Kuda <kuda2012@gmail.com>
2020-07-18 22:53:19 -0500

    simple login logout funcitonality added

--- app.py
+++ app.py
@@ -97,3 +97,33 @@ def signup():
 
     else:
         return render_template('signup.html', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    # IMPLEMENT THIS
+    user = User.query.get(session[CURR_USER_KEY])
+    flash(f"See you later, {user.username}", "success")
+    do_logout()
+    return redirect("/")
