205b5d81d8a1863e9b59910161e34cb78b8e3469
Kuda <kuda2012@gmail.com>
2020-07-29 01:51:19 -0500

    secrets removed

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import requests
 from forms import BookConditionsForm, UserForm, LoginForm, EditUsernameForm, EditUserPasswordForm, DeleteUserForm, SearchSavedBooks
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
-from secrets import SECRET_KEY
 from models import db, User, connect_db, SavedBooks
 import os
 import re
@@ -22,7 +21,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "secret")
 
 toolbar = DebugToolbarExtension(app)
 
