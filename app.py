aa1d1e7279cfd9d88fd3376f48cfd53a3f8f9d2f
Kuda <kuda2012@gmail.com>
2020-07-29 00:03:56 -0500

    changed origin

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ CURR_USER_KEY = "curr_user"
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgres:///book_db'))
+    os.environ.get('DATABASE_URL', "postgresql://postgres:postgres@127.0.0.1:5432/book_db"))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
