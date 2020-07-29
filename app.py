e5655115dd5318c72eb9b0d851491fd79c0652dc
Kuda <kuda2012@gmail.com>
2020-07-29 02:06:07 -0500

    set secret key

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "secret1")
 
 toolbar = DebugToolbarExtension(app)
 
