52cd9f1ccc1c50b23d166b5873bb2be24821a132
Kuda <kuda2012@gmail.com>
2020-07-30 19:16:53 -0500

    changed var to let

--- static/search_results.js
+++ static/search_results.js
@@ -12,7 +12,7 @@ const cardsContainer = document.getElementById("cardsContainer");
 const flashContainer = document.getElementById("flashContainer");
 const paginateButtons = Array.from(document.getElementsByClassName("paginate"));
 const $target = $("html,body");
-const userID;
+var userID = "";
 if (document.getElementById("userLoggedIn")) {
   userID = document.getElementById("userLoggedIn").getAttribute("data-user-id");
 }
